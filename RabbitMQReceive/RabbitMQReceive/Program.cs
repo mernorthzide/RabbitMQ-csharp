c749f8826514caba6e7ddd63aef023d7b37a0b6e
mernorthzide <mern_k@hotmail.com>
2017-02-10 10:49:13 +0700

    * Initial Upload

---
+++ RabbitMQReceive/RabbitMQReceive/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQReceive
+{
+    internal class Program
+    {
+        private const string QueueName = "ChatMessage";
+        private const string ExchangeName = "SendMessage";
+        private const string RoutingKey = "";
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private static IModel _chanel;
+
+        private static void Main(string[] args)
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "ChatAdmin",
+                Password = "1234",
+                VirtualHost = "Chat"
+            };
+            _connection = _factory.CreateConnection();
+            _chanel = _connection.CreateModel();
+            _chanel.QueueDeclare(QueueName, true, false, false, null);
+            _chanel.QueueBind(QueueName, ExchangeName, RoutingKey, null);
+
+            var consumer = new EventingBasicConsumer(_chanel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                _chanel.BasicAck(ea.DeliveryTag, false);
+
+                Console.WriteLine(" [x] {0}", message);
+            };
+
+            _chanel.BasicConsume(QueueName, false, consumer);
+        }
+    }
+}
---
+++ RabbitMQSend/RabbitMQSend/Program.cs
@@ -0,0 +1,46 @@
+﻿using NLipsum.Core;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQSend
+{
+    internal class Program
+    {
+        private const string QueueName = "ChatMessage";
+        private const string ExchangeName = "SendMessage";
+        private const string RoutingKey = "";
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private static IModel _chanel;
+
+        private static void Main(string[] args)
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "ChatAdmin",
+                Password = "1234",
+                VirtualHost = "Chat"
+            };
+            _connection = _factory.CreateConnection();
+            _chanel = _connection.CreateModel();
+            _chanel.QueueDeclare(QueueName, true, false, false, null);
+            _chanel.QueueBind(QueueName, ExchangeName, RoutingKey, null);
+
+            for (var i = 1; i <= 10000; i++)
+            {
+                var lipsum = LipsumGenerator.Generate(1);
+
+                Console.WriteLine("No. " + i);
+
+                // Add to queue
+                var chatEncode = Encoding.ASCII.GetBytes(i + lipsum);
+                _chanel.BasicPublish(ExchangeName, RoutingKey, null, chatEncode);
+            }
+        }
+    }
+}
